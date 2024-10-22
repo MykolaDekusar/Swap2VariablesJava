6cced3af9a4d46a034e9ca0162949c791e9b9fc9
MykolaDekusar <ndekusar97@gmail.com>
2024-10-22 17:06:42 +0200

    Scambio di dati tra 2 variabili in Java

--- VariablesSwap/src/def/Main.java
+++ VariablesSwap/src/def/Main.java
@@ -4,7 +4,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int x = 20;
+		int y = 10;
+		System.out.println("X vale: "+x);
+		System.out.println("Y vale: "+y);
+		System.out.println("Ora proviamo a cambiare i valori usando una terza variabile");
+		int z = x;
+		x=y;
+		y=z;
+		System.out.println("X vale: "+x);
+		System.out.println("Y vale: "+y);
+		
 	}
 
 }
